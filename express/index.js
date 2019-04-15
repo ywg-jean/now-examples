2ab5ad8e4424c7faf926b1425de59c6061884971
Mark <markdrew53@gmail.com>
2019-04-15 17:05:56 -0500

    Warning message deleted (#343)

--- express/index.js
+++ express/index.js
@@ -8,7 +8,7 @@ app.use(helmet())
 
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
-    res.send(200, `
+    res.status(200).send(`
         <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
         <h2>Go to <a href="/about">/about</a></h2>
     `)
