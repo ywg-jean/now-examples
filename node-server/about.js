b0c109e5ffc717674e20abbed7915411e091d6c8
Matthew Sweeney <mail@msweeneydev.com>
2019-05-31 21:53:14 +0100

    update node-server example (#384)
    * update node-server example
    
    * change readme header

--- node-server/about.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHeader(200, {"Content-Type": "text/html"});
-  res.end('This is the About Page <hr> <a href="/">Navigate to Home</a>');
-});
-
-server.listen();
--- node-server/index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHeader(200, {"Content-Type": "text/html"});
-  res.end('This is a Node.js server running on Now 2.0 <hr> <a href="/about">Navigate to About Page</a>');
-});
-
-server.listen();
